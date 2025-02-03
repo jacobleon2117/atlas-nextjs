17cc31a74eb5a48075b01a7a492bb819e4a66111
jacobleon2117 <jacobleon2117@gmail.com>
2025-02-02 23:19:56 -0600

    init commit

--- components/CreateTopicForm.tsx
+++ components/CreateTopicForm.tsx
@@ -20,12 +20,12 @@ export default function CreateTopicForm() {
         </div>
       </div>
       <div className="mt-6 flex justify-end gap-4">
-        <a
+        <link
           href="/ui"
           className="flex h-10 items-center rounded-lg bg-gray-100 px-4 text-sm font-medium text-gray-600 transition-colors hover:bg-gray-200"
         >
           Cancel
-        </a>
+        </link>
         <button type="submit">Create Topic</button>
       </div>
     </form>
