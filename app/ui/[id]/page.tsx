96e6fd3c3c796d1341786eff5eb1a9852105e990
jacobleon2117 <jacobleon2117@gmail.com>
2025-02-04 14:45:46 -0600

    still trying to fix error

--- app/ui/[id]/page.tsx
+++ app/ui/[id]/page.tsx
@@ -1,5 +1,7 @@
-export default async function Page({ params }: { params: { id: string } }) {
+export default async function Page({ params }: { params: Promise<{ id: string }> }) {
+  const { id } = await params;
+  
   await new Promise((r) => setTimeout(r, 3000));
 
-  return <div>Topic Page: {params.id}</div>;
+  return <div>Topic Page: {id}</div>;
 }
