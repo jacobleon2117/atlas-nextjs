01308d7ebfde0d39f6fdea78ea3555c77cd245c6
jacobleon2117 <jacobleon2117@gmail.com>
2025-02-12 14:42:40 -0600

    fixing

--- app/api/questions/[id]/answers/route.ts
+++ app/api/questions/[id]/answers/route.ts
@@ -1,7 +1,10 @@
 import { fetchAnswers } from "@/lib/data";
-import { NextResponse } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 
-export async function GET({ params }: { params: { id: string } }) {
+export async function GET(
+  request: NextRequest,
+  { params }: { params: { id: string } }
+) {
   try {
     const answers = await fetchAnswers(params.id);
     return NextResponse.json(answers);
